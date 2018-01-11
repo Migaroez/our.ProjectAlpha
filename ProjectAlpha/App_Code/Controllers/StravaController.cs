9f1028e1a9747208ffd658ea719b0a98ed9f9b47
Sven Geusens <geusens@hotmail.com>
2018-01-11 21:08:49 +0100

    Initial commit

---
+++ ProjectAlpha/App_Code/Controllers/StravaController.cs
@@ -0,0 +1,109 @@
+﻿using ProjectAlpha.App_Code.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+
+namespace ProjectAlpha.App_Code.Controllers
+{
+    public class StravaController : SurfaceController
+    {
+        public ActionResult Index()
+        {
+
+            IEnumerable<StravaModel> activities = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://www.strava.com/api/v3/athlete/");
+                //HTTP Get
+                var responseTask = client.GetAsync("activites");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IEnumerable<StravaModel>>(); // Changed to IEnumerable since your return type is a Ienumerable anyway (if you are going to run over the collection multiple times, use a List instead)
+                    readTask.Wait(); // this is blocking code https://msdn.microsoft.com/en-us/library/dd235635(v=vs.110).aspx See below for a working async copy
+
+                    activities = readTask.Result;
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    activities = Enumerable.Empty<StravaModel>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+
+            return View(activities);
+        }
+
+        public async Task<ActionResult> IndexAsync()
+        {
+
+            IEnumerable<StravaModel> activities = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://www.strava.com/api/v3/athlete/");
+                //HTTP Get
+                var responseTask = client.GetAsync("activites");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    activities = await result.Content.ReadAsAsync<IEnumerable<StravaModel>>(); 
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    activities = Enumerable.Empty<StravaModel>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+
+            return View("Index", activities);
+        }
+
+        public ActionResult IndexTest()
+        {
+
+            var activities = new List<StravaModel>
+            {
+                new StravaModel
+                {
+                    athlete = new Athlete
+                    {
+                        id = 1
+                    },
+                    average_heartrate = 0.1f
+                },
+                new StravaModel
+                {
+                    athlete = new Athlete
+                    {
+                        id = 2
+                    },
+                    average_heartrate = 0.2f
+                }
+            };
+
+
+            return View("Index",activities);
+        }
+    }
+
+}
---
+++ ProjectAlpha/App_Code/Models/StravaModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectAlpha.App_Code.Models
+{
+
+    public class StravaModel
+    {
+        public int id { get; set; }
+        public int resource_state { get; set; }
+        public string external_id { get; set; }
+        public int? upload_id { get; set; }
+        public Athlete athlete { get; set; }
+        public string name { get; set; }
+        public float distance { get; set; }
+        public int moving_time { get; set; }
+        public int elapsed_time { get; set; }
+        public float total_elevation_gain { get; set; }
+        public string type { get; set; }
+        public DateTime start_date { get; set; }
+        public DateTime start_date_local { get; set; }
+        public string timezone { get; set; }
+        public float utc_offset { get; set; }
+        public float[] start_latlng { get; set; }
+        public float[] end_latlng { get; set; }
+        public string location_city { get; set; }
+        public string location_state { get; set; }
+        public string location_country { get; set; }
+        public float? start_latitude { get; set; }
+        public float? start_longitude { get; set; }
+        public int achievement_count { get; set; }
+        public int kudos_count { get; set; }
+        public int comment_count { get; set; }
+        public int athlete_count { get; set; }
+        public int photo_count { get; set; }
+        public Map map { get; set; }
+        public bool trainer { get; set; }
+        public bool commute { get; set; }
+        public bool manual { get; set; }
+        public bool _private { get; set; }
+        public bool flagged { get; set; }
+        public string gear_id { get; set; }
+        public bool? from_accepted_tag { get; set; }
+        public float average_speed { get; set; }
+        public float max_speed { get; set; }
+        public float average_watts { get; set; }
+        public float kilojoules { get; set; }
+        public bool device_watts { get; set; }
+        public bool has_heartrate { get; set; }
+        public float elev_high { get; set; }
+        public float elev_low { get; set; }
+        public int pr_count { get; set; }
+        public int total_photo_count { get; set; }
+        public bool has_kudoed { get; set; }
+        public int? workout_type { get; set; }
+        public float average_cadence { get; set; }
+        public float average_heartrate { get; set; }
+        public float max_heartrate { get; set; }
+
+    }
+
+    public class Athlete
+    {
+        public int id { get; set; }
+        public int resource_state { get; set; }
+    }
+
+    public class Map
+    {
+        public string id { get; set; }
+        public string summary_polyline { get; set; }
+        public int resource_state { get; set; }
+    }
+
+}
